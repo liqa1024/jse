825b24a28e3fcb9fbf451526a52a5f6e532c9803
CHanzyLazer <55473143+CHanzyLazer@users.noreply.github.com>
2023-05-05 20:53:57 +0800

    错误输入时的提示

--- src/main/java/com/guan/Main.java
+++ src/main/java/com/guan/Main.java
@@ -9,6 +9,12 @@ import com.guan.script.Groovy;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
+        // 没有输入时给出使用方法
+        if (args.length == 0) {
+            System.out.println("Usage: jTool path/to/script.groovy");
+            System.out.println("Or you can also using another scripting language such as MATLAB or Python with Py4J and import jTool-*.jar");
+            return;
+        }
         // 默认执行 groovy 脚本
         Groovy.run(args[0]);
     }
