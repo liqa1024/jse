09f01270160f8b67d1987599e66cc2121b78776d
CHanzyLazer <55473143+CHanzyLazer@users.noreply.github.com>
2023-11-15 15:25:07 +0800

    配位数计算

--- release/script/groovy/test/mpc/testVoronoi1.groovy
+++ release/script/groovy/test/mpc/testVoronoi1.groovy
@@ -4,18 +4,19 @@ import jtool.code.UT
 import jtool.code.collection.ArrayLists
 import jtool.math.vector.LogicalVector
 import jtool.math.vector.Vectors
+import jtool.plot.Plotters
 import jtool.vasp.XDATCAR
 
 import static jtool.code.UT.Par.*
 import static jtool.code.UT.Code.*
-import static jtool.code.UT.Plot.*
 
 def dump = XDATCAR.read('lmp/.lll-in/XDATCAR');
 // 此数据需要每帧都进行一次 wrap
 for (data in dump) data.opt().wrap2this();
 
-// 统计原子体积变化趋势
+// 统计原子体积和配位数的变化趋势
 def allAtomicVolume = ArrayLists.from(dump.size(), {Vectors.zeros(dump.atomNum())})
+def coordination    = ArrayLists.from(dump.size(), {Vectors.zeros(dump.atomNum())})
 
 // 借助一下 MFPC 将原子按照相同的 id 排序；虽然对于 XDATCAR 是不必要的
 UT.Timer.tic();
@@ -24,6 +25,7 @@ try (def mfpc = dump.getMFPC(0.001)) {
         try (def mpc = mfpc.getMPC(i)) {
             def voronoi = mpc.calVoronoi();
             allAtomicVolume[i].fill({int j -> voronoi[j].atomicVolume()});
+            coordination   [i].fill({int j -> voronoi[j].coordination()});
         }
     }
 }
@@ -31,12 +33,22 @@ UT.Timer.toc('voronoi');
 
 
 // 绘图
+def plt1 = Plotters.get();
+def plt2 = Plotters.get();
+
 def data = dump[0];
 def typeLegend = LogicalVector.zeros(data.atomTypeNum());
 for (j in range(data.atomNum())) {
     def type = data.pickAtom(j).type();
-    plot(1..dump.size(), (allAtomicVolume*.get(j)), typeLegend[type-1] ? null : "type-$type").color(type);
+    plt1.plot(1..dump.size(), (allAtomicVolume*.get(j)), typeLegend[type-1] ? null : "type-$type").color(type);
+    plt2.plot(1..dump.size(), (coordination*.get(j))   , typeLegend[type-1] ? null : "type-$type").color(type);
     typeLegend[type-1] = true;
 }
 
-xrange(0, dump.size()+1);
+plt1.xrange(0, dump.size()+1);
+plt2.xrange(0, dump.size()+1);
+plt1.xlabel('frame').ylabel('allAtomicVolume');
+plt2.xlabel('frame').ylabel('coordination');
+plt1.show();
+plt2.show();
+
