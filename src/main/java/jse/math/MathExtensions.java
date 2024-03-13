bef6df0eacbbffe2484c41b6762a76050150bbb5
CHanzyLazer <55473143+CHanzyLazer@users.noreply.github.com>
2024-03-13 16:43:11 +0800

    MathExtensions  修掉实验接口，不关键就是

--- src/main/java/jse/math/MathExtensions.java
+++ src/main/java/jse/math/MathExtensions.java
@@ -6,14 +6,12 @@ import jse.math.vector.IComplexVector;
 import jse.math.vector.IIntVector;
 import jse.math.vector.ILogicalVector;
 import jse.math.vector.IVector;
-import org.jetbrains.annotations.ApiStatus;
 
 /**
  * 用于 Groovy 使用的扩展方法，
  * 用于对数字增加一些扩展运算
  * @author liqa
  */
-@ApiStatus.Experimental
 public class MathExtensions {
     public static IVector plus    (Number aLHS, IVector aRHS) {return aRHS.plus              (aLHS.doubleValue());}
     public static IVector minus   (Number aLHS, IVector aRHS) {return aRHS.operation().lminus(aLHS.doubleValue());}
