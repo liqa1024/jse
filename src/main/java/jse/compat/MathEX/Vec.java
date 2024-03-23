6a10dcb0dbe8d78b84de1ef1726ade7732766486
CHanzyLazer <55473143+CHanzyLazer@users.noreply.github.com>
2024-03-23 14:36:13 +0800

    移除严重冗余过时的求解方程和对应的Func3接口，现在没有这个需求了
    如果还需要求解，除了可以手动使用Vector来实现，也可等后续Tensor实现后统一实现Func2和Func3

--- src/main/java/jse/compat/MathEX/Vec.java
+++
@@ -1,8 +0,0 @@
-package jse.compat.MathEX;
-
-import jse.math.MathEX;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.VisibleForTesting;
-
-@VisibleForTesting @Deprecated @ApiStatus.ScheduledForRemoval
-public final class Vec extends MathEX.Vec {}
