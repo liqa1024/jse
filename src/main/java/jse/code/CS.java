cb08809a00e6bf236e5a08cdf35446758e7161fa
CHanzyLazer <55473143+CHanzyLazer@users.noreply.github.com>
2025-01-14 20:35:02 +0800

    3.4.1

--- src/main/java/jse/code/CS.java
+++ src/main/java/jse/code/CS.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
  */
 public class CS {
     /** version of jse */
-    public final static String VERSION = "3.4.0c";
+    public final static String VERSION = "3.4.1";
     
     /** a Random generator so I don't need to instantiate a new one all the time. */
     public final static Random RANDOM = new Random();
