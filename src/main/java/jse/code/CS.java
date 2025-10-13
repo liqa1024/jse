2ea0979fa873e5246a6c211972fc230ce5c14d4c
CHanzyLazer <chanzylazer@qq.com>
2025-10-13 13:21:45 +0800

    3.12.2

--- src/main/java/jse/code/CS.java
+++ src/main/java/jse/code/CS.java
@@ -30,8 +30,8 @@ import java.util.regex.Pattern;
  */
 public class CS {
     /** version of jse */
-    public final static String VERSION = "3.12.1b";
-    public final static int VERSION_NUMBER = 3_12_01_01;
+    public final static String VERSION = "3.12.2";
+    public final static int VERSION_NUMBER = 3_12_02_00;
     
     /** 内部使用的全局随机数生成器 */
     @ApiStatus.Internal public final static Random RANDOM_ = new Random();
