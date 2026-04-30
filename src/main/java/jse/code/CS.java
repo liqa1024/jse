399942fa9991c9555204e455e006638b6a807347
Qing'an Li <chanzylazer@qq.com>
2026-05-01 03:19:14 +0800

    4.0.0

--- src/main/java/jse/code/CS.java
+++ src/main/java/jse/code/CS.java
@@ -30,8 +30,8 @@ import java.util.regex.Pattern;
  */
 public class CS {
     /** version of jse */
-    public final static String VERSION = "3.99.1b";
-    public final static int VERSION_NUMBER = 3_99_01_01;
+    public final static String VERSION = "4.0.0";
+    public final static int VERSION_NUMBER = 4_00_00_00;
     
     /** 内部使用的全局随机数生成器 */
     @ApiStatus.Internal public final static Random RANDOM_ = new Random();
