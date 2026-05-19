e6ae882bacb81a59d937aeeff5b475d16f378204
Qing'an Li <chanzylazer@qq.com>
2026-05-19 14:52:07 +0800

    FixedTimer 的 getMillis 和 getNanos 行为修正

--- src/main/java/jse/code/timer/FixedTimer.java
+++ src/main/java/jse/code/timer/FixedTimer.java
@@ -7,8 +7,8 @@ package jse.code.timer;
 public class FixedTimer implements ITimer {
     private long mTime;
     public FixedTimer() {mTime = System.nanoTime();}
-    @Override public long getNanos() {return mTime;}
-    @Override public long getMillis() {return Math.round(mTime / 1.0e6);}
-    @Override public double get() {return (System.nanoTime() - mTime) / 1.0e9;}
+    @Override public long getNanos() {return System.nanoTime()-mTime;}
+    @Override public long getMillis() {return Math.round((System.nanoTime()-mTime) / 1.0e6);}
+    @Override public double get() {return (System.nanoTime()-mTime) / 1.0e9;}
     @Override public void reset() {mTime = System.nanoTime();}
 }
