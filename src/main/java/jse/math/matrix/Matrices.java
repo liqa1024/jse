968ef6b4152cfe10fb6cb02d3f301d56980a0a2a
Qing'an Li <chanzylazer@qq.com>
2026-05-23 00:24:47 +0800

    部分遗留命名调整

--- src/main/java/jse/math/matrix/Matrices.java
+++ src/main/java/jse/math/matrix/Matrices.java
@@ -19,12 +19,12 @@ public class Matrices {
     private Matrices() {}
     
     public static RowMatrix ones(int aSize) {return RowMatrix.ones(aSize);}
-    public static RowMatrix ones(int aRowNum, int aColNum) {return RowMatrix.ones(aRowNum, aColNum);}
+    public static RowMatrix ones(int aNumRows, int aNumCols) {return RowMatrix.ones(aNumRows, aNumCols);}
     public static RowMatrix zeros(int aSize) {return RowMatrix.zeros(aSize);}
-    public static RowMatrix zeros(int aRowNum, int aColNum) {return RowMatrix.zeros(aRowNum, aColNum);}
+    public static RowMatrix zeros(int aNumRows, int aNumCols) {return RowMatrix.zeros(aNumRows, aNumCols);}
     public static RowMatrix NaN(int aSize) {return NaN(aSize, aSize);}
-    public static RowMatrix NaN(int aRowNum, int aColNum) {
-        RowMatrix rMatrix = zeros(aRowNum, aColNum);
+    public static RowMatrix NaN(int aNumRows, int aNumCols) {
+        RowMatrix rMatrix = zeros(aNumRows, aNumCols);
         rMatrix.fill(Double.NaN);
         return rMatrix;
     }
@@ -50,16 +50,16 @@ public class Matrices {
         if (aUnsignedWarning && aNDArray.isUnsigned()) {
             UT.Code.warning("Input numpy is unsigned, which is not supported in jse, so the original signed value will be directly obtained");
         }
-        final int tRowNum = tDims[0], tColNum = tDims[1];
+        final int tNumRows = tDims[0], tNumCols = tDims[1];
         Object tData = aNDArray.getData();
         if (tData instanceof double[]) {
-            return new RowMatrix(tRowNum, tColNum, (double[])tData);
+            return new RowMatrix(tNumRows, tNumCols, (double[])tData);
         } else
         if (tData instanceof int[]) {
-            return new RowIntMatrix(tRowNum, tColNum, (int[])tData);
+            return new RowIntMatrix(tNumRows, tNumCols, (int[])tData);
         } else
         if (tData instanceof boolean[]) {
-            return new RowLogicalMatrix(tRowNum, tColNum, (boolean[])tData);
+            return new RowLogicalMatrix(tNumRows, tNumCols, (boolean[])tData);
         } else {
             throw new UnsupportedOperationException("Invalid numpy dtype: " + tData.getClass().getName());
         }
@@ -82,11 +82,11 @@ public class Matrices {
     
     
     public static RowMatrix from(int aSize, IMatrixGetter aMatrixGetter) {return fromDouble(aSize, aMatrixGetter);}
-    public static RowMatrix from(int aRowNum, int aColNum, IMatrixGetter aMatrixGetter) {return fromDouble(aRowNum, aColNum, aMatrixGetter);}
+    public static RowMatrix from(int aNumRows, int aNumCols, IMatrixGetter aMatrixGetter) {return fromDouble(aNumRows, aNumCols, aMatrixGetter);}
     public static RowMatrix from(IMatrix aMatrix) {return fromDouble(aMatrix);}
     public static RowMatrix fromDouble(int aSize, IMatrixGetter aMatrixGetter) {return fromDouble(aSize, aSize, aMatrixGetter);}
-    public static RowMatrix fromDouble(int aRowNum, int aColNum, IMatrixGetter aMatrixGetter) {
-        RowMatrix rMatrix = zeros(aRowNum, aColNum);
+    public static RowMatrix fromDouble(int aNumRows, int aNumCols, IMatrixGetter aMatrixGetter) {
+        RowMatrix rMatrix = zeros(aNumRows, aNumCols);
         rMatrix.fill(aMatrixGetter);
         return rMatrix;
     }
@@ -99,14 +99,14 @@ public class Matrices {
     public static RowMatrix from(int aSize, @ClosureParams(value=FromString.class, options={"int,int"}) final Closure<? extends Number> aGroovyTask) {
         return fromDouble(aSize, aGroovyTask);
     }
-    public static RowMatrix from(int aRowNum, int aColNum, @ClosureParams(value=FromString.class, options={"int,int"}) final Closure<? extends Number> aGroovyTask) {
-        return fromDouble(aRowNum, aGroovyTask);
+    public static RowMatrix from(int aNumRows, int aNumCols, @ClosureParams(value=FromString.class, options={"int,int"}) final Closure<? extends Number> aGroovyTask) {
+        return fromDouble(aNumRows, aGroovyTask);
     }
     public static RowMatrix fromDouble(int aSize, @ClosureParams(value=FromString.class, options={"int,int"}) final Closure<? extends Number> aGroovyTask) {
         return fromDouble(aSize, (i, j) -> UT.Code.doubleValue(aGroovyTask.call(i, j)));
     }
-    public static RowMatrix fromDouble(int aRowNum, int aColNum, @ClosureParams(value=FromString.class, options={"int,int"}) final Closure<? extends Number> aGroovyTask) {
-        return fromDouble(aRowNum, aColNum, (i, j) ->UT.Code.doubleValue( aGroovyTask.call(i, j)));
+    public static RowMatrix fromDouble(int aNumRows, int aNumCols, @ClosureParams(value=FromString.class, options={"int,int"}) final Closure<? extends Number> aGroovyTask) {
+        return fromDouble(aNumRows, aNumCols, (i, j) ->UT.Code.doubleValue( aGroovyTask.call(i, j)));
     }
     
     public static RowMatrix from(Collection<?> aRows) {return fromDouble(aRows);}
@@ -114,42 +114,42 @@ public class Matrices {
     public static RowMatrix fromCols(Collection<?> aCols) {return fromColsDouble(aCols);}
     public static RowMatrix fromDouble(Collection<?> aRows) {return fromRowsDouble(aRows);}
     public static RowMatrix fromRowsDouble(Collection<?> aRows) {
-        int tColNum;
+        int tNumCols;
         if (aRows.isEmpty()) {
-            tColNum = 0;
+            tNumCols = 0;
         } else {
             Object tFirst = UT.Code.first(aRows);
             if (tFirst instanceof Collection) {
-                tColNum = ((Collection<?>)tFirst).size();
+                tNumCols = ((Collection<?>)tFirst).size();
             } else if (tFirst instanceof IVector) {
-                tColNum = ((IVector)tFirst).size();
+                tNumCols = ((IVector)tFirst).size();
             } else if (tFirst instanceof double[]) {
-                tColNum = ((double[])tFirst).length;
+                tNumCols = ((double[])tFirst).length;
             } else {
                 throw new IllegalArgumentException("Type of Row Must be Collection<? extends Number>, IVector or double[]");
             }
         }
-        RowMatrix rMatrix = zeros(aRows.size(), tColNum);
+        RowMatrix rMatrix = zeros(aRows.size(), tNumCols);
         rMatrix.fillWithRows(aRows);
         return rMatrix;
     }
     public static RowMatrix fromColsDouble(Collection<?> aCols) {
-        int tRowNum;
+        int tNumRows;
         if (aCols.isEmpty()) {
-            tRowNum = 0;
+            tNumRows = 0;
         } else {
             Object tFirst = UT.Code.first(aCols);
             if (tFirst instanceof Collection) {
-                tRowNum = ((Collection<?>)tFirst).size();
+                tNumRows = ((Collection<?>)tFirst).size();
             } else if (tFirst instanceof IVector) {
-                tRowNum = ((IVector)tFirst).size();
+                tNumRows = ((IVector)tFirst).size();
             } else if (tFirst instanceof double[]) {
-                tRowNum = ((double[])tFirst).length;
+                tNumRows = ((double[])tFirst).length;
             } else {
                 throw new IllegalArgumentException("Type of Column Must be Collection<? extends Number>, IVector or double[]");
             }
         }
-        RowMatrix rMatrix = zeros(tRowNum, aCols.size());
+        RowMatrix rMatrix = zeros(tNumRows, aCols.size());
         rMatrix.fillWithCols(aCols);
         return rMatrix;
     }
@@ -157,8 +157,8 @@ public class Matrices {
     
     /// LogicalMatrix stuffs
     public static RowLogicalMatrix fromBoolean(int aSize, ILogicalMatrixGetter aMatrixGetter) {return fromBoolean(aSize, aSize, aMatrixGetter);}
-    public static RowLogicalMatrix fromBoolean(int aRowNum, int aColNum, ILogicalMatrixGetter aMatrixGetter) {
-        RowLogicalMatrix rMatrix = RowLogicalMatrix.zeros(aRowNum, aColNum);
+    public static RowLogicalMatrix fromBoolean(int aNumRows, int aNumCols, ILogicalMatrixGetter aMatrixGetter) {
+        RowLogicalMatrix rMatrix = RowLogicalMatrix.zeros(aNumRows, aNumCols);
         rMatrix.fill(aMatrixGetter);
         return rMatrix;
     }
@@ -171,56 +171,56 @@ public class Matrices {
     public static RowLogicalMatrix fromBoolean(int aSize, @ClosureParams(value=FromString.class, options={"int,int"}) final Closure<Boolean> aGroovyTask) {
         return fromBoolean(aSize, aGroovyTask::call);
     }
-    public static RowLogicalMatrix fromBoolean(int aRowNum, int aColNum, @ClosureParams(value=FromString.class, options={"int,int"}) final Closure<Boolean> aGroovyTask) {
-        return fromBoolean(aRowNum, aColNum, aGroovyTask::call);
+    public static RowLogicalMatrix fromBoolean(int aNumRows, int aNumCols, @ClosureParams(value=FromString.class, options={"int,int"}) final Closure<Boolean> aGroovyTask) {
+        return fromBoolean(aNumRows, aNumCols, aGroovyTask::call);
     }
     
     public static RowLogicalMatrix fromBoolean(Collection<?> aRows) {return fromRowsBoolean(aRows);}
     public static RowLogicalMatrix fromRowsBoolean(Collection<?> aRows) {
-        int tColNum;
+        int tNumCols;
         if (aRows.isEmpty()) {
-            tColNum = 0;
+            tNumCols = 0;
         } else {
             Object tFirst = UT.Code.first(aRows);
             if (tFirst instanceof Collection) {
-                tColNum = ((Collection<?>)tFirst).size();
+                tNumCols = ((Collection<?>)tFirst).size();
             } else if (tFirst instanceof ILogicalVector) {
-                tColNum = ((ILogicalVector)tFirst).size();
+                tNumCols = ((ILogicalVector)tFirst).size();
             } else if (tFirst instanceof boolean[]) {
-                tColNum = ((boolean[])tFirst).length;
+                tNumCols = ((boolean[])tFirst).length;
             } else {
                 throw new IllegalArgumentException("Type of Row Must be Collection<Boolean>, ILogicalVector or boolean[]");
             }
         }
-        RowLogicalMatrix rMatrix = RowLogicalMatrix.zeros(aRows.size(), tColNum);
+        RowLogicalMatrix rMatrix = RowLogicalMatrix.zeros(aRows.size(), tNumCols);
         rMatrix.fillWithRows(aRows);
         return rMatrix;
     }
     public static RowLogicalMatrix fromColsBoolean(Collection<?> aCols) {
-        int tRowNum;
+        int tNumRows;
         if (aCols.isEmpty()) {
-            tRowNum = 0;
+            tNumRows = 0;
         } else {
             Object tFirst = UT.Code.first(aCols);
             if (tFirst instanceof Collection) {
-                tRowNum = ((Collection<?>)tFirst).size();
+                tNumRows = ((Collection<?>)tFirst).size();
             } else if (tFirst instanceof ILogicalVector) {
-                tRowNum = ((ILogicalVector)tFirst).size();
+                tNumRows = ((ILogicalVector)tFirst).size();
             } else if (tFirst instanceof boolean[]) {
-                tRowNum = ((boolean[])tFirst).length;
+                tNumRows = ((boolean[])tFirst).length;
             } else {
                 throw new IllegalArgumentException("Type of Column Must be Collection<Boolean>, ILogicalVector or boolean[]");
             }
         }
-        RowLogicalMatrix rMatrix = RowLogicalMatrix.zeros(tRowNum, aCols.size());
+        RowLogicalMatrix rMatrix = RowLogicalMatrix.zeros(tNumRows, aCols.size());
         rMatrix.fillWithCols(aCols);
         return rMatrix;
     }
     
     /// IntMatrix stuffs
     public static RowIntMatrix fromInt(int aSize, IIntMatrixGetter aMatrixGetter) {return fromInt(aSize, aSize, aMatrixGetter);}
-    public static RowIntMatrix fromInt(int aRowNum, int aColNum, IIntMatrixGetter aMatrixGetter) {
-        RowIntMatrix rMatrix = RowIntMatrix.zeros(aRowNum, aColNum);
+    public static RowIntMatrix fromInt(int aNumRows, int aNumCols, IIntMatrixGetter aMatrixGetter) {
+        RowIntMatrix rMatrix = RowIntMatrix.zeros(aNumRows, aNumCols);
         rMatrix.fill(aMatrixGetter);
         return rMatrix;
     }
@@ -233,48 +233,48 @@ public class Matrices {
     public static RowIntMatrix fromInt(int aSize, @ClosureParams(value=FromString.class, options={"int,int"}) final Closure<Integer> aGroovyTask) {
         return fromInt(aSize, aGroovyTask::call);
     }
-    public static RowIntMatrix fromInt(int aRowNum, int aColNum, @ClosureParams(value=FromString.class, options={"int,int"}) final Closure<Integer> aGroovyTask) {
-        return fromInt(aRowNum, aColNum, aGroovyTask::call);
+    public static RowIntMatrix fromInt(int aNumRows, int aNumCols, @ClosureParams(value=FromString.class, options={"int,int"}) final Closure<Integer> aGroovyTask) {
+        return fromInt(aNumRows, aNumCols, aGroovyTask::call);
     }
     
     public static RowIntMatrix fromInt(Collection<?> aRows) {return fromRowsInt(aRows);}
     public static RowIntMatrix fromRowsInt(Collection<?> aRows) {
-        int tColNum;
+        int tNumCols;
         if (aRows.isEmpty()) {
-            tColNum = 0;
+            tNumCols = 0;
         } else {
             Object tFirst = UT.Code.first(aRows);
             if (tFirst instanceof Collection) {
-                tColNum = ((Collection<?>)tFirst).size();
+                tNumCols = ((Collection<?>)tFirst).size();
             } else if (tFirst instanceof IIntVector) {
-                tColNum = ((IIntVector)tFirst).size();
+                tNumCols = ((IIntVector)tFirst).size();
             } else if (tFirst instanceof int[]) {
-                tColNum = ((int[])tFirst).length;
+                tNumCols = ((int[])tFirst).length;
             } else {
                 throw new IllegalArgumentException("Type of Row Must be Collection<Integer>, IIntVector or int[]");
             }
         }
-        RowIntMatrix rMatrix = RowIntMatrix.zeros(aRows.size(), tColNum);
+        RowIntMatrix rMatrix = RowIntMatrix.zeros(aRows.size(), tNumCols);
         rMatrix.fillWithRows(aRows);
         return rMatrix;
     }
     public static RowIntMatrix fromColsInt(Collection<?> aCols) {
-        int tRowNum;
+        int tNumRows;
         if (aCols.isEmpty()) {
-            tRowNum = 0;
+            tNumRows = 0;
         } else {
             Object tFirst = UT.Code.first(aCols);
             if (tFirst instanceof Collection) {
-                tRowNum = ((Collection<?>)tFirst).size();
+                tNumRows = ((Collection<?>)tFirst).size();
             } else if (tFirst instanceof IIntVector) {
-                tRowNum = ((IIntVector)tFirst).size();
+                tNumRows = ((IIntVector)tFirst).size();
             } else if (tFirst instanceof int[]) {
-                tRowNum = ((int[])tFirst).length;
+                tNumRows = ((int[])tFirst).length;
             } else {
                 throw new IllegalArgumentException("Type of Column Must be Collection<Integer>, IIntVector or int[]");
             }
         }
-        RowIntMatrix rMatrix = RowIntMatrix.zeros(tRowNum, aCols.size());
+        RowIntMatrix rMatrix = RowIntMatrix.zeros(tNumRows, aCols.size());
         rMatrix.fillWithCols(aCols);
         return rMatrix;
     }
