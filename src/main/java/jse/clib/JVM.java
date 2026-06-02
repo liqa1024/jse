61d070668b2c9790c2a319f9d88fe1667b6f2e78
Qing'an Li <chanzylazer@qq.com>
2026-06-02 21:43:56 +0800

    JVM 支持获取 jni_md.h 所在目录

--- src/main/java/jse/clib/JVM.java
+++ src/main/java/jse/clib/JVM.java
@@ -1,7 +1,12 @@
 package jse.clib;
 
+import jse.code.IO;
+
+import java.io.IOException;
+
 import static jse.code.Conf.LIB_NAME_IN;
 import static jse.code.Conf.LLIB_NAME_IN;
+import static jse.code.OS.IS_MAC;
 import static jse.code.OS.IS_WINDOWS;
 import static jse.code.OS.JAVA_HOME_DIR;
 
@@ -12,6 +17,8 @@ import static jse.code.OS.JAVA_HOME_DIR;
 public class JVM {
     /** 当前 {@link JVM} 的 include 目录，结尾一定存在 {@code '/'} */
     public final static String INCLUDE_DIR;
+    /** 当前 {@link JVM} 的 jni_md.h 所在目录，结尾一定存在 {@code '/'} */
+    public final static String INCLUDE_MD_DIR;
     /** 当前 {@link JVM} 的动态库所在的目录，结尾一定存在 {@code '/'} */
     public final static String LIB_DIR;
     /** 当前 {@link JVM} 的动态库路径 */
@@ -23,6 +30,27 @@ public class JVM {
     
     static {
         INCLUDE_DIR = JAVA_HOME_DIR + "include/";
+        // 优先使用系统判断，没有则自动检测任何目录
+        String tMdName = IS_WINDOWS ? "win32" : (IS_MAC ? "darwin" : "linux");
+        String tIncludeMdDir = INCLUDE_DIR + tMdName+"/";
+        if (!IO.exists(tIncludeMdDir + "jni_md.h")) {
+            String[] tNames;
+            try {
+                tNames = IO.list(INCLUDE_DIR);
+            } catch (IOException e) {
+                throw new RuntimeException("Fail to det list of \"" + INCLUDE_DIR + "\"");
+            }
+            tIncludeMdDir = null;
+            for (String tName : tNames) {
+                String tIncludeMdDir_ = INCLUDE_DIR + tName + "/";
+                if (IO.isDir(tIncludeMdDir_)) {
+                    tIncludeMdDir = tIncludeMdDir_;
+                    break;
+                }
+            }
+        }
+        INCLUDE_MD_DIR = tIncludeMdDir;
+        // lib 目录获取
         String tLibDir = JAVA_HOME_DIR + "bin/server/";
         String tLibName = LIB_NAME_IN(tLibDir, "jvm");
         if (tLibName == null) {
@@ -37,7 +65,9 @@ public class JVM {
             tLibDir = JAVA_HOME_DIR + "lib/client/";
             tLibName = LIB_NAME_IN(tLibDir, "jvm");
         }
-        if (tLibName == null) throw new RuntimeException("No jvm lib in '$JAVA_HOME/bin/server/', '$JAVA_HOME/bin/client/', '$JAVA_HOME/lib/server/' or '$JAVA_HOME/lib/client/'");
+        if (tLibName == null) {
+            throw new RuntimeException("No jvm lib in '$JAVA_HOME/bin/server/', '$JAVA_HOME/bin/client/', '$JAVA_HOME/lib/server/' or '$JAVA_HOME/lib/client/'");
+        }
         LIB_DIR = tLibDir;
         LIB_PATH = tLibDir + tLibName;
         if (!IS_WINDOWS) {
